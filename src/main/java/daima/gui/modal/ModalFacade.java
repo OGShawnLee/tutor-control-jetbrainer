000c48a1b9c0955301ea1fd21f99d48b13d67b89
OGShawnLee <OGShawnLee@gmail.com>
2025-12-08 11:02:08 -0600

    refactor!: simplify ModalFacade and remove Modal class

--- src/main/java/daima/gui/modal/ModalFacade.java
+++ src/main/java/daima/gui/modal/ModalFacade.java
@@ -3,81 +3,48 @@ package daima.gui.modal;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 import java.io.IOException;
+import java.util.Objects;
 
 import daima.common.ExceptionHandler;
-import daima.gui.AlertFacade;
-import daima.gui.controller.ContextController;
+import daima.common.UserDisplayableException;
 
-/** ModalFacade is a utility class that simplifies the creation and display of modals in the GUI.
- * It handles the loading of FXML resources, displaying modals, and managing context for controllers.
- */
 public class ModalFacade {
   private final static Logger LOGGER = LogManager.getLogger(ModalFacade.class);
-  private final Modal modal;
 
-  /**
-   * Constructs a ModalFacade with the given configuration.
-   *
-   * @param configuration the configuration for the modal
-   * @throws IOException if there is an error loading the FXML resource
-   */
-  private ModalFacade(ModalFacadeConfiguration configuration) throws IOException {
-    this.modal = new Modal(configuration);
+  public static FXMLLoader createFXMLLoader(String resourceFileName) throws IOException {
+    return new FXMLLoader(
+      Objects.requireNonNull(ModalFacade.class.getResource("/" + resourceFileName + ".fxml"))
+    );
   }
 
-  private Modal getModal() {
-    return modal;
-  }
-
-  private void display() {
-    getModal().showAndWait();
-  }
-
-  public static void createAndDisplay(ModalFacadeConfiguration configuration) {
+  public static <T> T displayModal(ModalFacadeConfiguration configuration) throws UserDisplayableException {
     try {
-      ModalFacade modalFacade = new ModalFacade(configuration);
-      modalFacade.display();
-    } catch (IOException e) {
-      AlertFacade.showErrorAndWait(
-        ExceptionHandler.handleGUILoadIOException(LOGGER, e).getMessage()
+      FXMLLoader loader = createFXMLLoader(configuration.getResourceFileName());
+      Parent parent = loader.load();
+      Scene scene = new Scene(parent);
+      Stage modalStage = new Stage();
+
+      modalStage.setTitle(configuration.getTitle());
+      modalStage.setScene(scene);
+      modalStage.setResizable(false);
+      modalStage.initModality(Modality.APPLICATION_MODAL);
+
+      configuration.getOnClose().ifPresent(runnable ->
+        modalStage.setOnHidden(event -> runnable.run())
       );
-    }
-  }
 
-  /**
-   * Displays a modal with a specific context.
-   *
-   * @param context the context to be passed to the controller
-   * @param <T>     the type of the context
-   */
-  private <T> void displayContextModal(T context) {
-    Modal modal = getModal();
-    ContextController<T> controller = modal.getController();
-    controller.setContext(context);
-    modal.showAndWait();
-  }
+      modalStage.show();
 
-  /**
-   * Creates and displays a modal with a specific context.
-   * This method is useful for scenarios where the modal needs to operate with a specific context,
-   * such as when working on a database record.
-   * <p>
-   * It initializes the controller with the provided context and displays the modal.
-   * This method is generic and can be used with any type of context.
-   * It offers a convenient way to create and display modals without needing to
-   * manually set up the controller and modal each time.
-   *
-   * @param configuration the configuration for the modal
-   * @param context       the context to be passed to the controller
-   * @param <T>           the type of the context
-   */
-  public static <T> void createAndDisplayContextModal(ModalFacadeConfiguration configuration, T context) {
-    try {
-      ModalFacade modalFacade = new ModalFacade(configuration);
-      modalFacade.displayContextModal(context);
+      return loader.getController();
     } catch (IOException e) {
-      AlertFacade.showErrorAndWait(
+      throw new UserDisplayableException(
         ExceptionHandler.handleGUILoadIOException(LOGGER, e).getMessage()
       );
     }
