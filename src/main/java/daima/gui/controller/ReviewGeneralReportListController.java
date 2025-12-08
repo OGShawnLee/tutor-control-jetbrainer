b578aa686753d046f5140188b873ee6862ecd058
OGShawnLee <OGShawnLee@gmail.com>
2025-12-08 11:20:35 -0600

    feat: add 'Review General Report' use case

--- src/main/java/daima/gui/controller/ReviewGeneralReportListController.java
+++ src/main/java/daima/gui/controller/ReviewGeneralReportListController.java
@@ -1,6 +1,7 @@
 package daima.gui.controller;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -8,13 +9,17 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import daima.business.AuthClient;
 import daima.business.dao.ReportDAO;
 import daima.business.dto.ReportDTO;
 import daima.business.enumeration.ReportType;
 import daima.business.enumeration.TutoringSessionKind;
 import daima.business.enumeration.TutoringSessionPlanState;
-import daima.gui.modal.ModalFacade;
-import daima.gui.modal.ModalFacadeConfiguration;
+import daima.business.service.GeneralReportService;
+import daima.common.UserDisplayableException;
+import daima.gui.AlertFacade;
+
+import java.util.ArrayList;
 
 public class ReviewGeneralReportListController extends Controller {
   @FXML
@@ -38,17 +43,41 @@ public class ReviewGeneralReportListController extends Controller {
   }
 
   public void configureTableColumns() {
-    columnPeriod.setCellValueFactory(new PropertyValueFactory<>("period"));
-    columnTutoringSessionKind.setCellValueFactory(new PropertyValueFactory<>("tutoringSessionKind"));
+    columnPeriod.setCellValueFactory(new PropertyValueFactory<>("periodDTO"));
+    columnTutoringSessionKind.setCellValueFactory(new PropertyValueFactory<>("sessionKind"));
     columnState.setCellValueFactory(new PropertyValueFactory<>("state"));
     columnCoordinatorName.setCellValueFactory(new PropertyValueFactory<>("nameStaff"));
     columnFormattedCreatedAt.setCellValueFactory(new PropertyValueFactory<>("formattedCreatedAt"));
   }
 
+  public void configureSearch(
+    ObservableList<ReportDTO> reportDTOObservableList
+  ) {
+    useConfigureSearch(fieldSearch, reportDTOObservableList, tableGeneralReport);
+  }
+
   public void setTableItems() {
-    tableGeneralReport.setItems(
-      FXCollections.observableList(ReportDAO.getInstance().getAllByProgram(0, ReportType.GENERAL_REPORT))
-    );
+    try {
+      ArrayList<ReportDTO> reportDTOList = ReportDAO.getInstance().getAllByProgram(
+        AuthClient.getInstance().getCurrentStaff().getProgramCoordinated().getID(),
+        ReportType.GENERAL_REPORT
+      );
+
+      if (reportDTOList.isEmpty()) {
+        AlertFacade.showErrorAndWait(
+          "No es posible mostrar elementos porque no hay ninguno registrado en el sistema aún."
+        );
+      }
+
+      ObservableList<ReportDTO> reportDTOObservableList = FXCollections.observableList(reportDTOList);
+
+      tableGeneralReport.setItems(reportDTOObservableList);
+      configureSearch(reportDTOObservableList);
+    } catch (UserDisplayableException e) {
+      AlertFacade.showErrorAndWait(
+        "No ha sido posible recuperar información debido a un error en la base de datos, intente de nuevo más tarde."
+      );
+    }
   }
 
   public void onClickViewGeneralReport() {
@@ -62,20 +91,59 @@ public class ReviewGeneralReportListController extends Controller {
   }
 
   public void onClickManageReport() {
-    getSelectedItemFromTable(tableGeneralReport).ifPresent(generalReportDTO ->
-      ModalFacade.createAndDisplayContextModal(
-        new ModalFacadeConfiguration(
-          "Modificar Reporte General",
-          "GUIRegisterGeneralReportModal",
-          this::setTableItems
-        ),
-        generalReportDTO
-      )
+    getSelectedItemFromTable(tableGeneralReport).ifPresent(it ->
+      RegisterGeneralReportController.displayManageReportModal(this::setTableItems, it)
     );
   }
 
+  private void deleteReport(ReportDTO reportDTO) {
+    boolean shallDelete = AlertFacade.showConfirmationAndWait(
+      "¿Está seguro de que desea eliminar este reporte permantemente? No podra recuperarlo."
+    );
+
+    if (shallDelete) {
+      try {
+        boolean failed = GeneralReportService.getInstance().deleteOne(reportDTO);
+
+        if (failed) {
+          AlertFacade.showErrorAndWait("No ha sido posible eliminar el reporte.");
+        } else {
+          AlertFacade.showSuccessAndWait("El reporte ha sido eliminado exitosamente.");
+          setTableItems();
+        }
+      } catch (UserDisplayableException e) {
+        AlertFacade.showErrorAndWait(e);
+      }
+    }
+  }
+
+  public void onClickDeleteReport() {
+    getSelectedItemFromTable(tableGeneralReport).ifPresent(this::deleteReport);
+  }
+
+  private void sendReport(ReportDTO reportDTO) {
+    boolean shallSend = AlertFacade.showConfirmationAndWait(
+      "¿Está seguro de que desea enviar este reporte? Ya no podrá editarlo ni eliminarlo."
+    );
+
+    if (shallSend) {
+      try {
+        boolean failed = GeneralReportService.getInstance().sendOne(reportDTO);
+
+        if (failed) {
+          AlertFacade.showErrorAndWait("No ha sido posible enviar el reporte.");
+        } else {
+          AlertFacade.showSuccessAndWait("El reporte ha sido enviado exitosamente.");
+          setTableItems();
+        }
+      } catch (UserDisplayableException e) {
+        AlertFacade.showErrorAndWait(e);
+      }
+    }
+  }
+
   public void onClickSendReport() {
-    // TODO: Add Send General Report
+    getSelectedItemFromTable(tableGeneralReport).ifPresent(this::sendReport);
   }
 
   public static void navigateToGeneralReportListPage(Stage currentStage) {
