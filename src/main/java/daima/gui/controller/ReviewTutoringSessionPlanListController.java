bc7bb7a70c788cec26928af157ac2ab8b1d15263
OGShawnLee <OGShawnLee@gmail.com>
2025-12-08 11:21:13 -0600

    feat: add 'Review Tutoring Session Plan List' use case

--- src/main/java/daima/gui/controller/ReviewTutoringSessionPlanListController.java
+++ src/main/java/daima/gui/controller/ReviewTutoringSessionPlanListController.java
@@ -1,6 +1,7 @@
 package daima.gui.controller;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -8,10 +9,18 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import daima.business.AuthClient;
+import daima.business.dao.PeriodDAO;
 import daima.business.dao.TutoringSessionPlanDAO;
+import daima.business.dto.StaffDTO;
+import daima.business.dto.PeriodDTO;
 import daima.business.dto.TutoringSessionPlanDTO;
 import daima.business.enumeration.TutoringSessionKind;
 import daima.business.enumeration.TutoringSessionPlanState;
+import daima.common.UserDisplayableException;
+import daima.gui.AlertFacade;
+
+import java.util.ArrayList;
 
 public class ReviewTutoringSessionPlanListController extends Controller {
   @FXML
@@ -25,8 +34,6 @@ public class ReviewTutoringSessionPlanListController extends Controller {
   @FXML
   private TableColumn<TutoringSessionPlanDTO, String> columnAppointmentDate;
   @FXML
-  private TableColumn<TutoringSessionPlanDTO, String> columnCoordinatorName;
-  @FXML
   private TableColumn<TutoringSessionPlanDTO, String> columnFormattedCreatedAt;
   @FXML
   private TextField fieldSearch;
@@ -37,18 +44,42 @@ public class ReviewTutoringSessionPlanListController extends Controller {
   }
 
   public void configureTableColumns() {
-    columnPeriod.setCellValueFactory(new PropertyValueFactory<>("period"));
+    columnPeriod.setCellValueFactory(new PropertyValueFactory<>("periodDTO"));
     columnKind.setCellValueFactory(new PropertyValueFactory<>("kind"));
     columnState.setCellValueFactory(new PropertyValueFactory<>("state"));
     columnAppointmentDate.setCellValueFactory(new PropertyValueFactory<>("appointmentDate"));
-    columnCoordinatorName.setCellValueFactory(new PropertyValueFactory<>("nameCoordinator"));
     columnFormattedCreatedAt.setCellValueFactory(new PropertyValueFactory<>("formattedCreatedAt"));
   }
 
+  public void configureSearch(
+    ObservableList<TutoringSessionPlanDTO> planDTOObservableList
+  ) {
+    useConfigureSearch(fieldSearch, planDTOObservableList, tableTutoringSessionPlan);
+  }
+
   public void setTableItems() {
-    tableTutoringSessionPlan.setItems(
-      FXCollections.observableList(TutoringSessionPlanDAO.getInstance().getAllByProgram(0))
-    );
+    try {
+      StaffDTO currentStaffDTO = AuthClient.getInstance().getCurrentStaff();
+      PeriodDTO currentPeriodDTO = PeriodDAO.getInstance().getCurrentPeriod();
+      ArrayList<TutoringSessionPlanDTO> planDTOList = TutoringSessionPlanDAO.getInstance().getAllByProgramAndPeriod(
+        currentStaffDTO.getProgramCoordinated().getID(),
+        currentPeriodDTO
+      );
+
+      if (planDTOList.isEmpty()) {
+        AlertFacade.showErrorAndWait(
+          "No es posible mostrar elementos porque no hay ninguno registrado en el sistema aún."
+        );
+      }
+
+      ObservableList<TutoringSessionPlanDTO> planDTOObservableList = FXCollections.observableList(planDTOList);
+      tableTutoringSessionPlan.setItems(planDTOObservableList);
+      configureSearch(planDTOObservableList);
+    } catch (UserDisplayableException e) {
+      AlertFacade.showErrorAndWait(
+        "No ha sido posible recuperar información debido a un error en la base de datos, intente de nuevo más tarde."
+      );
+    }
   }
 
   public void onClickRegisterTutoringSessionPlan() {
