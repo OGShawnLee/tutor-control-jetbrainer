75a8384cf0610bb6ce6d5bcf76100f6a9becaa73
OGShawnLee <OGShawnLee@gmail.com>
2025-12-08 11:23:36 -0600

    progress: add 'Update Profile' additional use case

--- src/main/java/daima/gui/controller/UpdateProfileController.java
+++ src/main/java/daima/gui/controller/UpdateProfileController.java
@@ -1,13 +1,17 @@
 package daima.gui.controller;
 
-import daima.business.AuthClient;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import daima.business.AuthClient;
+import daima.business.validator.ValidationResult;
+import daima.business.validator.Validator;
+import daima.business.dao.StaffDAO;
 import daima.business.dto.StaffDTO;
-import daima.business.enumeration.StaffRole;
+import daima.common.InvalidFieldException;
+import daima.common.UserDisplayableException;
 import daima.gui.AlertFacade;
 import daima.gui.modal.ModalFacade;
 import daima.gui.modal.ModalFacadeConfiguration;
@@ -28,41 +32,31 @@ public class UpdateProfileController extends Controller {
   @FXML
   private TextField fieldIDWorker;
   @FXML
-  private ComboBox<StaffRole> fieldStaffRole;
+  private TextField fieldFormattedRoles;
   @FXML
-  private TextField fieldPassword;
+  private PasswordField fieldPassword;
   @FXML
   private Label labelTagPassword;
   @FXML
-  private TextField fieldConfirmPassword;
+  private PasswordField fieldConfirmPassword;
   @FXML
   private Label labelTagConfirmPassword;
   private StaffDTO editStaffDTO;
 
-  public void setContext(StaffDTO data) {
-    editStaffDTO = data;
-    loadEditData();
-  }
-
   public void initialize() {
+    editStaffDTO = AuthClient.getInstance().getCurrentStaff();
     cleanErrorLabels();
-    configureFieldStaffRole();
-    setContext(AuthClient.getInstance().getCurrentStaff());
+    configureFormData();
   }
 
-  private void loadEditData() {
+  private void configureFormData() {
     if (editStaffDTO == null) return;
 
     fieldName.setText(editStaffDTO.getName());
     fieldLastName.setText(editStaffDTO.getLastName());
     fieldEmail.setText(editStaffDTO.getEmail());
     fieldIDWorker.setText(editStaffDTO.getIDWorker());
-    fieldStaffRole.setValue(editStaffDTO.getRole());
-  }
-
-  private void configureFieldStaffRole() {
-    fieldStaffRole.getItems().setAll(StaffRole.values());
-    fieldStaffRole.setValue(StaffRole.TUTOR);
+    fieldFormattedRoles.setText(editStaffDTO.getFormattedRoles());
   }
 
   private void cleanErrorLabels() {
@@ -74,22 +68,89 @@ public class UpdateProfileController extends Controller {
   }
 
   private boolean isInvalidData() {
-    return true;
-  }
+    boolean isInvalidData = false;
 
-  private StaffDTO getStaffDTOFromInput() {
-    return new StaffDTO(
+    ValidationResult<String> result = Validator.getNameValidationResult(
       fieldName.getText(),
-      fieldLastName.getText(),
-      fieldEmail.getText(),
-      fieldIDWorker.getText(),
-      fieldStaffRole.getValue()
+      "Nombre",
+      3,
+      64
     );
+    if (result.isInvalid()) {
+      labelTagName.setText(result.getError());
+      isInvalidData = true;
+    }
+
+    result = Validator.getNameValidationResult(
+      fieldLastName.getText(),
+      "Apellidos",
+      3,
+      256
+    );
+    if (result.isInvalid()) {
+      labelTagLastName.setText(result.getError());
+      isInvalidData = true;
+    }
+
+    result = Validator.getEmailValidationResult(fieldEmail.getText());
+    if (result.isInvalid()) {
+      labelTagEmail.setText(result.getError());
+      isInvalidData = true;
+    }
+
+    if (isInvalidPassword()) {
+      isInvalidData = true;
+    }
+
+    return isInvalidData;
   }
 
-  private void updateStaff() {
-    // TODO: Add Proper Update Staff
-    AuthClient.getInstance().setCurrentStaff(getStaffDTOFromInput());
+  private boolean isInvalidPassword() {
+    String passwordText = fieldPassword.getText();
+    String confirmPasswordText = fieldConfirmPassword.getText();
+
+    boolean hasPasswordError = false;
+    boolean isPasswordProvided = (passwordText != null && !passwordText.trim().isEmpty());
+
+    if (isPasswordProvided) {
+      ValidationResult<String> passwordResult = Validator.getStrongPasswordValidationResult(passwordText);
+      if (passwordResult.isInvalid()) {
+        labelTagPassword.setText(passwordResult.getError());
+        hasPasswordError = true;
+      }
+
+      ValidationResult<String> confirmPasswordResult = Validator.getStrongPasswordValidationResult(confirmPasswordText);
+      if (confirmPasswordResult.isInvalid()) {
+        labelTagConfirmPassword.setText(confirmPasswordResult.getError());
+        hasPasswordError = true;
+      }
+
+      if (!hasPasswordError) {
+        if (!passwordText.equals(confirmPasswordText)) {
+          labelTagConfirmPassword.setText("Las contraseñas no coinciden.");
+          hasPasswordError = true;
+        }
+      }
+    } else {
+      if (confirmPasswordText != null && !confirmPasswordText.trim().isEmpty()) {
+        labelTagConfirmPassword.setText("Si no establece la contraseña, el campo de confirmación debe estar vacío.");
+        hasPasswordError = true;
+      }
+    }
+
+    return hasPasswordError;
+  }
+
+  private StaffDTO getStaffDTOFromInput() throws InvalidFieldException {
+    editStaffDTO.setName(fieldName.getText());
+    editStaffDTO.setLastName(fieldLastName.getText());
+    editStaffDTO.setEmail(fieldEmail.getText());
+    return editStaffDTO;
+  }
+
+  private void updateStaff() throws InvalidFieldException, UserDisplayableException {
+    StaffDAO.getInstance().updateOne(getStaffDTOFromInput());
+    AlertFacade.showSuccessAndWait("Su cuenta de miembro de personal ha sido actualizada exitosamente.");
   }
 
   public void onClickUpdateProfile() {
@@ -97,17 +158,24 @@ public class UpdateProfileController extends Controller {
 
     if (isInvalidData()) return;
 
-    updateStaff();
-    AlertFacade.showSuccessAndWait("Su cuenta de miembro de personal ha sido actualizada exitosamente.");
+    try {
+      updateStaff();
+    } catch (UserDisplayableException | InvalidFieldException e) {
+      AlertFacade.showErrorAndWait(e.getMessage());
+    }
   }
 
   public static void displayUpdateStaffModal(Runnable onClose) {
-    ModalFacade.createAndDisplay(
-      new ModalFacadeConfiguration(
-        "Modificar Cuenta de Personal",
-        "GUIUpdateProfileModal",
-        onClose
-      )
-    );
+    try {
+      ModalFacade.displayModal(
+        new ModalFacadeConfiguration(
+          "Modificar Cuenta de Personal",
+          "GUIUpdateProfileModal",
+          onClose
+        )
+      );
+    } catch (UserDisplayableException e) {
+      AlertFacade.showErrorAndWait(e);
+    }
   }
 }
