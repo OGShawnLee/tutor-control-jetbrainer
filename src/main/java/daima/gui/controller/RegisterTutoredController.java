f5ace3038a548e2147d9bc6a4d6cd02ee18d0d5a
OGShawnLee <OGShawnLee@gmail.com>
2025-12-08 11:17:49 -0600

    feat: add 'Manage Tutored' use case

--- src/main/java/daima/gui/controller/RegisterTutoredController.java
+++ src/main/java/daima/gui/controller/RegisterTutoredController.java
@@ -5,16 +5,22 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import daima.business.dao.ProgramDAO;
+import daima.business.dao.TutoredDAO;
 import daima.business.dto.ProgramDTO;
 import daima.business.dto.TutoredDTO;
+import daima.business.validator.ValidationResult;
+import daima.business.validator.Validator;
+import daima.business.service.ProgramService;
+import daima.common.InvalidFieldException;
+import daima.common.UserDisplayableException;
 import daima.gui.AlertFacade;
 import daima.gui.modal.ModalFacade;
 import daima.gui.modal.ModalFacadeConfiguration;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
-public class RegisterTutoredController extends Controller implements ContextController<TutoredDTO> {
+public class RegisterTutoredController extends Controller {
   @FXML
   private Label title;
   @FXML
@@ -26,10 +32,6 @@ public class RegisterTutoredController extends Controller implements ContextCont
   @FXML
   private Label labelTagLastName;
   @FXML
-  private TextField fieldEmail;
-  @FXML
-  private Label labelTagEmail;
-  @FXML
   private TextField fieldEnrollment;
   @FXML
   private Label labelTagEnrollment;
@@ -39,78 +41,203 @@ public class RegisterTutoredController extends Controller implements ContextCont
   private Label labelTagProgram;
   private TutoredDTO editTutoredDTO;
 
-  @Override
-  public void setContext(TutoredDTO data) {
-    editTutoredDTO = data;
-    loadEditData();
+  public void setContext(ArrayList<ProgramDTO> programDTOList, TutoredDTO editTutoredDTO) {
+    this.editTutoredDTO = editTutoredDTO;
+    configureFieldProgram(programDTOList);
+    configureEditFormData();
     configureTitle();
   }
 
   public void initialize() {
     cleanErrorLabels();
-    configureFieldProgram();
   }
 
   private void configureTitle() {
-    if (editTutoredDTO == null) return;
-
-    title.setText("Modificar Tutorado");
+    if (editTutoredDTO == null) {
+      title.setText("Registrar Tutorado");
+    } else {
+      title.setText("Modificar Tutorado");
+    }
   }
 
-  private void loadEditData() {
+  private void configureEditFormData() {
     if (editTutoredDTO == null) return;
 
     fieldName.setText(editTutoredDTO.getName());
     fieldLastName.setText(editTutoredDTO.getLastName());
-    fieldEmail.setText(editTutoredDTO.getEmail());
     fieldEnrollment.setText(editTutoredDTO.getEnrollment());
+    configureEditFormDataProgramField();
   }
 
-  private void configureFieldProgram() {
-    ArrayList<ProgramDTO> programDTOList = ProgramDAO.getInstance().getAll();
+  private void configureEditFormDataProgramField() {
+    if (editTutoredDTO.getIDTutor().isPresent()) {
+      fieldProgram.setDisable(true);
+    }
 
+    for (ProgramDTO programDTO : fieldProgram.getItems()) {
+      if (programDTO.getID() == editTutoredDTO.getIDProgram()) {
+        fieldProgram.setValue(programDTO);
+        return;
+      }
+    }
+  }
+
+  private void configureFieldProgram(ArrayList<ProgramDTO> programDTOList) {
     fieldProgram.getItems().setAll(programDTOList);
-    fieldProgram.setValue(programDTOList.get(0));
   }
 
   private void cleanErrorLabels() {
     labelTagName.setText("");
     labelTagLastName.setText("");
-    labelTagEmail.setText("");
     labelTagEnrollment.setText("");
     labelTagProgram.setText("");
   }
 
   private boolean isInvalidData() {
+    boolean isInvalid = false;
+
+    ValidationResult<String> nameResult = Validator.getNameValidationResult(
+      fieldName.getText(), "Nombre", 3, 64
+    );
+    if (nameResult.isInvalid()) {
+      labelTagName.setText(nameResult.getError());
+      isInvalid = true;
+    }
+
+    ValidationResult<String> lastNameResult = Validator.getNameValidationResult(
+      fieldLastName.getText(), "Apellido", 3, 256
+    );
+    if (lastNameResult.isInvalid()) {
+      labelTagLastName.setText(lastNameResult.getError());
+      isInvalid = true;
+    }
+
+    ValidationResult<String> enrollmentResult = Validator.getEnrollmentValidationResult(fieldEnrollment.getText());
+    if (enrollmentResult.isInvalid()) {
+      labelTagEnrollment.setText(enrollmentResult.getError());
+      isInvalid = true;
+    }
+
+    if (fieldProgram.getValue() == null) {
+      labelTagProgram.setText("Debe seleccionar un Programa Educativo.");
+      isInvalid = true;
+    }
+
+    return isInvalid;
+  }
+
+  private TutoredDTO getTutoredDTOFromInput() throws InvalidFieldException {
+    if (editTutoredDTO == null) {
+      TutoredDTO tutoredDTO = new TutoredDTO();
+      tutoredDTO.setName(fieldName.getText().trim());
+      tutoredDTO.setLastName(fieldLastName.getText().trim());
+      tutoredDTO.setEnrollment(fieldEnrollment.getText().trim());
+      tutoredDTO.setIDProgram(fieldProgram.getValue().getID());
+      return tutoredDTO;
+    } else {
+      editTutoredDTO.setName(fieldName.getText().trim());
+      editTutoredDTO.setIDProgram(fieldProgram.getValue().getID());
+      editTutoredDTO.setLastName(fieldLastName.getText().trim());
+      editTutoredDTO.setEnrollment(fieldEnrollment.getText().trim());
+      return editTutoredDTO;
+    }
+  }
+
+  private void handleDuplicateTutoredVerification() throws UserDisplayableException {
+    if (editTutoredDTO == null || !editTutoredDTO.getEnrollment().equals(fieldEnrollment.getText())) {
+      Optional<TutoredDTO> existingTutoredDTO = TutoredDAO.getInstance().findOneByEnrollment(fieldEnrollment.getText());
+
+      if (existingTutoredDTO.isPresent()) {
+        labelTagEnrollment.setText("La matrícula le pertenece a otro tutorado.");
+        throw new UserDisplayableException("No ha sido posible registrar tutorado por que la matricula introducida ya se encuentra en uso.");
+      }
+    }
+  }
+
+  private boolean getUpdateConfirmation() {
+    String originalEnrollment = editTutoredDTO.getEnrollment();
+    String newEnrollment = fieldEnrollment.getText();
+
+    if (!originalEnrollment.equals(newEnrollment)) {
+      boolean shallUpdate = AlertFacade.showConfirmationAndWait(
+        String.format(
+          "¿Esta seguro de que desea cambiar la matrícula del tutorado de %s a %s?",
+          originalEnrollment,
+          newEnrollment
+        )
+      );
+
+      if (!shallUpdate) return false;
+    }
+
+    int originalProgramID = editTutoredDTO.getIDProgram();
+    if (originalProgramID != fieldProgram.getValue().getID()) {
+      return AlertFacade.showConfirmationAndWait(
+        String.format(
+          "¿Esta seguro de que desea cambiar el programa educativo del tutorado de %s a %s?",
+          editTutoredDTO.getProgramName(),
+          fieldProgram.getValue().getName()
+        )
+      );
+    }
+
     return true;
   }
 
-  private TutoredDTO getTutoredDTOFromInput() {
-    return new TutoredDTO(
-      fieldName.getText(),
-      fieldLastName.getText(),
-      fieldEmail.getText(),
-      fieldEnrollment.getText(),
-      fieldProgram.getValue().getID()
-    );
+  private void registerTutored() throws InvalidFieldException, UserDisplayableException {
+    TutoredDAO.getInstance().createOne(getTutoredDTOFromInput());
+    AlertFacade.showSuccessAndWait("El tutorado ha sido registrado exitosamente.");
+    close();
   }
 
-  private void registerTutored() {
-    // TODO: Add Register Tutored
+  private void updateTutored() throws InvalidFieldException, UserDisplayableException {
+    assert editTutoredDTO != null;
+
+    if (getUpdateConfirmation()) {
+      boolean failed = TutoredDAO.getInstance().updateOne(getTutoredDTOFromInput());
+
+      if (failed) {
+        AlertFacade.showErrorAndWait("No ha sido posible modificar el tutorado.");
+      } else {
+        AlertFacade.showSuccessAndWait("El tutorado ha sido modificado exitosamente.");
+        close();
+      }
+    }
   }
 
   public void onClickRegisterStaff() {
     cleanErrorLabels();
-    AlertFacade.showSuccessAndWait("El tutorado ha sido registrado exitosamente.");
+
+    if (isInvalidData()) return;
+
+    try {
+      handleDuplicateTutoredVerification();
+      if (editTutoredDTO == null) registerTutored();
+      else updateTutored();
+    } catch (InvalidFieldException | UserDisplayableException e) {
+      AlertFacade.showErrorAndWait(e.getMessage());
+    }
+  }
+
+  public static void displayManageTutoredModal(Runnable onClose, TutoredDTO tutoredDTO) {
+    try {
+      ArrayList<ProgramDTO> programDTOList = ProgramService.getInstance().getAllForRegistration();
+
+      RegisterTutoredController controller = ModalFacade.displayModal(
+        new ModalFacadeConfiguration(
+          String.format("%s Tutorado", tutoredDTO == null ? "Registrar" : "Modificar"),
+          "GUIRegisterTutoredModal",
+          onClose
+        )
+      );
+
+      controller.setContext(programDTOList, tutoredDTO);
+    } catch (UserDisplayableException e) {
+      AlertFacade.showErrorAndWait(e);
+    }
   }
 
   public static void displayRegisterTutoredModal(Runnable onClose) {
-    ModalFacade.createAndDisplay(
-      new ModalFacadeConfiguration(
-        "Registrar Tutorado",
-        "GUIRegisterTutoredModal",
-        onClose
-      )
-    );
+    displayManageTutoredModal(onClose, null);
   }
 }
