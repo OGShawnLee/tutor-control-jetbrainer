466a4cda7089c517a5d2fcab5b8987bfd33c07c6
OGShawnLee <OGShawnLee@gmail.com>
2025-12-08 11:12:33 -0600

    feat: add 'Register Program' and 'Manage Program' additional use case

--- src/main/java/daima/gui/controller/RegisterProgramController.java
+++ src/main/java/daima/gui/controller/RegisterProgramController.java
@@ -1,36 +1,50 @@
 package daima.gui.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import daima.business.dao.ProgramDAO;
+import daima.business.dao.StaffDAO;
 import daima.business.dto.ProgramDTO;
+import daima.business.dto.StaffDTO;
+import daima.business.enumeration.StaffRole;
+import daima.business.validator.Validator;
+import daima.business.validator.ValidationResult;
+import daima.business.service.ProgramService;
+import daima.common.InvalidFieldException;
+import daima.common.UserDisplayableException;
 import daima.gui.AlertFacade;
 import daima.gui.modal.ModalFacade;
 import daima.gui.modal.ModalFacadeConfiguration;
 
-public class RegisterProgramController extends Controller implements ContextController<ProgramDTO> {
+import java.util.Optional;
+
+public class RegisterProgramController extends Controller {
   @FXML
   private Label title;
+  @FXML
+  private TextField fieldAcronym;
+  @FXML
+  private Label labelTagAcronym;
   @FXML
   private TextField fieldName;
   @FXML
   private Label labelTagName;
   @FXML
-  private TextField fieldAcronym;
-  @FXML
-  private Label labelTagAcronym;
+  private ComboBox<StaffDTO> fieldCoordinator;
   private ProgramDTO editProgramDTO;
 
-  @Override
-  public void setContext(ProgramDTO data) {
-    editProgramDTO = data;
+  private void setContext(ProgramDTO editProgramDTO) {
+    this.editProgramDTO = editProgramDTO;
     loadEditData();
     configureTitle();
   }
 
   public void initialize() {
     cleanErrorLabels();
+    configureFieldCoordinator();
   }
 
   private void configureTitle() {
@@ -39,11 +53,27 @@ public class RegisterProgramController extends Controller implements ContextCont
     title.setText("Modificar Programa Educativo");
   }
 
+  private void configureFieldCoordinator() {
+    try {
+      fieldCoordinator.getItems().addAll(StaffDAO.getInstance().getAllByRole(StaffRole.COORDINATOR));
+    } catch (UserDisplayableException e) {
+      AlertFacade.showErrorAndWait(e.getMessage());
+    }
+  }
+
   private void loadEditData() {
     if (editProgramDTO == null) return;
 
     fieldName.setText(editProgramDTO.getName());
     fieldAcronym.setText(editProgramDTO.getAcronym());
+    editProgramDTO.getIDCoordinator().ifPresent(idCoordinator -> {
+      for (StaffDTO staffDTO : fieldCoordinator.getItems()) {
+        if (staffDTO.getID() == idCoordinator) {
+          fieldCoordinator.setValue(staffDTO);
+          break;
+        }
+      }
+    });
   }
 
   private void cleanErrorLabels() {
@@ -52,37 +82,131 @@ public class RegisterProgramController extends Controller implements ContextCont
   }
 
   private boolean isInvalidData() {
-    return true;
+    boolean isInvalidData = false;
+
+    ValidationResult<String> result = Validator.getFlexibleNameValidationResult(
+      fieldName.getText(),
+      "Nombre de Programa",
+      3,
+      128
+    );
+    if (result.isInvalid()) {
+      labelTagName.setText(result.getError());
+      isInvalidData = true;
+    }
+
+    result = Validator.getAcronymValidationResult(fieldAcronym.getText());
+    if (result.isInvalid()) {
+      labelTagAcronym.setText(result.getError());
+      isInvalidData = true;
+    }
+
+    return isInvalidData;
   }
 
   private ProgramDTO getProgramDTOFromInput() {
-    return new ProgramDTO(
-      fieldName.getText().trim(),
-      fieldAcronym.getText().trim()
+    ProgramDTO programDTO = this.editProgramDTO == null ? new ProgramDTO() : this.editProgramDTO;
+
+    programDTO.setName(fieldName.getText().trim());
+    programDTO.setAcronym(fieldAcronym.getText().trim());
+
+    if (fieldCoordinator.getValue() != null) {
+      programDTO.setIDCoordinator(fieldCoordinator.getValue().getID());
+    }
+
+    return programDTO;
+  }
+
+  private void handleDuplicateProgramVerification() throws UserDisplayableException {
+    if (editProgramDTO == null || !editProgramDTO.getAcronym().equalsIgnoreCase(fieldAcronym.getText().trim())) {
+      Optional<ProgramDTO> existingProgramDTO = ProgramDAO.getInstance().findOneByAcronym(fieldAcronym.getText().trim());
+
+      if (existingProgramDTO.isPresent()) {
+        throw new UserDisplayableException(
+          "No ha sido posible registrar programa educativo debido a que ya existe uno con ese acrónimo."
+        );
+      }
+    }
+  }
+
+  private void registerProgram() throws InvalidFieldException, UserDisplayableException {
+    assert editProgramDTO == null;
+
+    ProgramService.getInstance().createProgram(getProgramDTOFromInput());
+    AlertFacade.showSuccessAndWait("El programa educativo ha sido registrado exitosamente.");
+  }
+
+  private boolean getUpdateConfirmation() {
+    int oldIDCoordinator = editProgramDTO.getIDCoordinator().orElse(-1);
+    int newIDCoordinator = fieldCoordinator.getValue() != null ? fieldCoordinator.getValue().getID() : -1;
+
+    if (oldIDCoordinator == newIDCoordinator) {
+      return false;
+    }
+
+    return AlertFacade.showConfirmationAndWait(
+      "¿Está seguro que desea cambiar el coordinador del programa educativo?"
     );
   }
 
+  private void updateProgram() throws InvalidFieldException, UserDisplayableException {
+    assert editProgramDTO != null;
 
-  private void updateProgram() {
-    // TODO: Add Update Program
-  }
+    if (getUpdateConfirmation()) {
+      boolean failed = ProgramService.getInstance().updateProgram(getProgramDTOFromInput());
 
-  private void registerProgram() {
-    // TODO: Add Register Program
+      if (failed) {
+        AlertFacade.showErrorAndWait("No ha sido posible modificar el programa educativo.");
+      } else {
+        AlertFacade.showSuccessAndWait("El programa educativo ha sido modificado exitosamente.");
+      }
+    }
   }
 
   public void onClickRegisterProgram() {
     cleanErrorLabels();
-    AlertFacade.showSuccessAndWait("El programa educativo ha sido registrado exitosamente.");
+
+    if (isInvalidData()) return;
+
+    try {
+      handleDuplicateProgramVerification();
+      if (editProgramDTO == null) {
+        registerProgram();
+      } else {
+        updateProgram();
+      }
+    } catch (InvalidFieldException | UserDisplayableException e) {
+      AlertFacade.showErrorAndWait(e.getMessage());
+    }
   }
 
   public static void displayRegisterProgramModal(Runnable onClose) {
-    ModalFacade.createAndDisplay(
-      new ModalFacadeConfiguration(
-        "Registrar Programa Educativo",
-        "GUIRegisterProgramModal",
-        onClose
-      )
-    );
+    try {
+      ModalFacade.displayModal(
+        new ModalFacadeConfiguration(
+          "Registrar Programa Educativo",
+          "GUIRegisterProgramModal",
+          onClose
+        )
+      );
+    } catch (UserDisplayableException e) {
+      AlertFacade.showErrorAndWait(e);
+    }
+  }
+
+  public static void displayManageProgramModal(Runnable onClose, ProgramDTO programDTO) {
+    try {
+      RegisterProgramController controller = ModalFacade.displayModal(
+        new ModalFacadeConfiguration(
+          "Registrar Programa Educativo",
+          "GUIRegisterProgramModal",
+          onClose
+        )
+      );
+
+      controller.setContext(programDTO);
+    } catch (UserDisplayableException e) {
+      AlertFacade.showErrorAndWait(e);
+    }
   }
 }
