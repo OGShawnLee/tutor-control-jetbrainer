52d91ce123c66d0a740133caaad01a9c5b7be504
OGShawnLee <OGShawnLee@gmail.com>
2025-12-08 11:00:05 -0600

    feat: add ReportDAO

--- src/main/java/daima/business/dao/ReportDAO.java
+++ src/main/java/daima/business/dao/ReportDAO.java
@@ -1,25 +1,213 @@
 package daima.business.dao;
 
-import daima.business.dto.ReportDTO;
-import daima.business.enumeration.ReportType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Optional;
 
-public class ReportDAO {
-  private final static ReportDAO INSTANCE = new ReportDAO();
+import daima.business.dto.PeriodDTO;
+import daima.business.dto.ReportDTO;
+import daima.business.enumeration.ReportState;
+import daima.business.enumeration.ReportType;
+import daima.business.enumeration.Semester;
+import daima.business.enumeration.TutoringSessionKind;
+import daima.common.ExceptionHandler;
+import daima.common.UserDisplayableException;
+import daima.db.DBConnector;
+
+public class ReportDAO extends DAOShape<ReportDTO> {
+  private static final Logger LOGGER = LogManager.getLogger(ReportDAO.class);
+  private static final String CREATE_ONE_QUERY =
+    "INSERT INTO Report (staff_id, session_plan_id, content, type) VALUES (?, ?, ?, ?)";
+  private static final String FIND_ONE_GENERAL_REPORT_BY_SESSION_PLAN_QUERY =
+    "SELECT * FROM CompleteReportView WHERE type = 'GENERAL_REPORT' AND session_plan_id = ?";
+  private static final String GET_ALL_QUERY = "SELECT * FROM CompleteReportView WHERE type = ?";
+  private static final String GET_ALL_BY_PROGRAM_QUERY =
+    "SELECT * FROM CompleteReportView WHERE type = ? AND program_id = ?";
+  private static final String GET_ONE_QUERY = "SELECT * FROM CompleteReportView WHERE id = ? AND type = ?";
+  private static final String UPDATE_ONE_QUERY = "UPDATE Report SET content = ?, state = ? WHERE id = ?";
+  private static final String DELETE_ONE_QUERY = "DELETE FROM Report WHERE id = ?";
+  private static final ReportDAO INSTANCE = new ReportDAO();
 
   public static ReportDAO getInstance() {
     return INSTANCE;
   }
 
-  public ArrayList<ReportDTO> getAllByProgram(int idProgram, ReportType type) {
-    return new ArrayList<>(Arrays.asList(
-      new ReportDTO(),
-      new ReportDTO(),
-      new ReportDTO(),
-      new ReportDTO(),
-      new ReportDTO()
-    ));
+  @Override
+  public ReportDTO getDTOInstanceFromResultSet(ResultSet resultSet) throws SQLException {
+    ReportDTO reportDTO = new ReportDTO();
+    reportDTO.setID(resultSet.getInt("id"));
+    reportDTO.setIDStaff(resultSet.getInt("staff_id"));
+    reportDTO.setIDSessionPlan(resultSet.getInt("session_plan_id"));
+    reportDTO.setIDProgram(resultSet.getInt("program_id"));
+    reportDTO.setNameStaff(resultSet.getString("name_staff"));
+    reportDTO.setContent(resultSet.getString("content"));
+    reportDTO.setSessionKind(
+      TutoringSessionKind.valueOf(resultSet.getString("session_kind"))
+    );
+    reportDTO.setType(ReportType.valueOf(resultSet.getString("type")));
+    reportDTO.setState(ReportState.valueOf(resultSet.getString("state")));
+    reportDTO.setPeriodDTO(
+      new PeriodDTO(
+        resultSet.getInt("period_year"),
+        Semester.valueOf(resultSet.getString("period_semester"))
+      )
+    );
+    reportDTO.setCreatedAt(resultSet.getTimestamp("created_at").toLocalDateTime());
+    return reportDTO;
+  }
+
+  public void createOne(ReportDTO reportDTO) throws UserDisplayableException {
+    try (
+      Connection connection = DBConnector.getInstance().getConnection();
+      PreparedStatement statement = connection.prepareStatement(CREATE_ONE_QUERY)
+    ) {
+      statement.setInt(1, reportDTO.getIDStaff());
+      statement.setInt(2, reportDTO.getIDSessionPlan());
+      statement.setString(3, reportDTO.getContent());
+      statement.setString(4, reportDTO.getType().name());
+
+      statement.executeUpdate();
+    } catch (SQLException e) {
+      throw ExceptionHandler.handleSQLException(LOGGER, e, "No ha sido posible crear el reporte.");
+    }
+  }
+
+  public Optional<ReportDTO> findOneGeneralReportBySessionPlan(int idSessionPlan) throws UserDisplayableException {
+    try (
+      Connection connection = DBConnector.getInstance().getConnection();
+      PreparedStatement statement = connection.prepareStatement(FIND_ONE_GENERAL_REPORT_BY_SESSION_PLAN_QUERY)
+    ) {
+      statement.setInt(1, idSessionPlan);
+
+      try (ResultSet resultSet = statement.executeQuery()) {
+        if (resultSet.next()) {
+          return Optional.of(createDTOInstanceFromResultSet(resultSet));
+        } else {
+          return Optional.empty();
+        }
+      }
+    } catch (SQLException e) {
+      throw ExceptionHandler.handleSQLException(LOGGER, e, "No ha sido posible cargar el reporte.");
+    }
+  }
+
+  public ArrayList<ReportDTO> getAll(ReportType type) throws UserDisplayableException {
+    ArrayList<ReportDTO> reportDTOList = new ArrayList<>();
+
+    try (
+      Connection connection = DBConnector.getInstance().getConnection();
+      PreparedStatement statement = connection.prepareStatement(GET_ALL_QUERY)
+    ) {
+      statement.setString(1, type.name());
+
+      try (ResultSet resultSet = statement.executeQuery()) {
+        while (resultSet.next()) {
+          reportDTOList.add(createDTOInstanceFromResultSet(resultSet));
+        }
+      }
+
+      return reportDTOList;
+    } catch (SQLException e) {
+      throw ExceptionHandler.handleSQLException(LOGGER, e, "No ha sido posible cargar los reportes.");
+    }
+  }
+
+  public ArrayList<ReportDTO> getAllByProgram(int idProgram, ReportType type) throws UserDisplayableException {
+    ArrayList<ReportDTO> reportDTOList = new ArrayList<>();
+
+    try (
+      Connection connection = DBConnector.getInstance().getConnection();
+      PreparedStatement statement = connection.prepareStatement(GET_ALL_BY_PROGRAM_QUERY)
+    ) {
+      statement.setString(1, type.name());
+      statement.setInt(2, idProgram);
+
+      try (ResultSet resultSet = statement.executeQuery()) {
+        while (resultSet.next()) {
+          reportDTOList.add(createDTOInstanceFromResultSet(resultSet));
+        }
+      }
+
+      return reportDTOList;
+    } catch (SQLException e) {
+      throw ExceptionHandler.handleSQLException(LOGGER, e, "No ha sido posible cargar los reportes.");
+    }
+  }
+
+  public ReportDTO getOne(int idReport, ReportType type) throws UserDisplayableException {
+    try (
+      Connection connection = DBConnector.getInstance().getConnection();
+      PreparedStatement statement = connection.prepareStatement(GET_ONE_QUERY)
+    ) {
+      statement.setInt(1, idReport);
+      statement.setString(2, type.name());
+
+      try (ResultSet resultSet = statement.executeQuery()) {
+        if (resultSet.next()) {
+          return createDTOInstanceFromResultSet(resultSet);
+        }
+      }
+
+      throw new UserDisplayableException("El reporte solicitado no existe.");
+    } catch (SQLException e) {
+      throw ExceptionHandler.handleSQLException(LOGGER, e, "No ha sido posible cargar el reporte.");
+    }
+  }
+
+  public boolean updateOne(ReportDTO reportDTO) throws UserDisplayableException {
+    try (
+      Connection connection = DBConnector.getInstance().getConnection();
+      PreparedStatement statement = connection.prepareStatement(UPDATE_ONE_QUERY);
+    ) {
+      statement.setString(1, reportDTO.getContent());
+      statement.setString(2, reportDTO.getState().name());
+      statement.setInt(3, reportDTO.getID());
+
+      statement.executeUpdate();
+
+      boolean failed = statement.getUpdateCount() == -1;
+
+      if (failed) {
+        LOGGER.error(
+          "Actualización Fallida Inesperada de Reporte: {} {}",
+          reportDTO.getID(),
+          reportDTO.getType().name()
+        );
+      }
+
+      return failed;
+    } catch (SQLException e) {
+      throw ExceptionHandler.handleSQLException(LOGGER, e, "No ha sido posible actualizar el reporte.");
+    }
+  }
+
+  public boolean deleteOne(ReportDTO reportDTO) throws UserDisplayableException {
+    try (
+      Connection connection = DBConnector.getInstance().getConnection();
+      PreparedStatement statement = connection.prepareStatement(DELETE_ONE_QUERY);
+    ) {
+      statement.setInt(1, reportDTO.getID());
+      statement.executeUpdate();
+
+      boolean failed = statement.getUpdateCount() == -1;
+
+      if (failed) {
+        LOGGER.error(
+          "Eliminación Fallida Inesperada de Reporte: {} {}",
+          reportDTO.getID(),
+          reportDTO.getType().name()
+        );
+      }
+
+      return failed;
+    } catch (SQLException e) {
+      throw ExceptionHandler.handleSQLException(LOGGER, e, "No ha sido posible eliminar el reporte.");
+    }
   }
 }
--- src/main/java/daima/business/dto/ReportDTO.java
+++ src/main/java/daima/business/dto/ReportDTO.java
@@ -2,52 +2,110 @@ package daima.business.dto;
 
 import daima.business.enumeration.ReportState;
 import daima.business.enumeration.ReportType;
-import daima.business.enumeration.Semester;
 import daima.business.enumeration.TutoringSessionKind;
 
 import java.time.LocalDateTime;
 
-public class ReportDTO implements Record {
+public class ReportDTO implements Record, Searchable {
   private int id;
+  private int idStaff;
+  private int idSessionPlan;
+  private int idProgram;
+  private int idResponse;
+  private String nameStaff;
   private String content;
-  private PeriodDTO period;
+  private TutoringSessionKind sessionKind;
   private ReportType type;
   private ReportState state;
-  private TutoringSessionKind tutoringSessionKind;
+  private PeriodDTO periodDTO;
   private LocalDateTime createdAt;
-  private int idStaff;
-  private String nameStaff;
-  private int idResponse;
-  private int idProgram;
-
-  public ReportDTO() {
-    this.state = ReportState.DRAFT;
-    this.type = ReportType.GENERAL_REPORT;
-    this.tutoringSessionKind = TutoringSessionKind.FIRST_TUTORING_SESSION;
-    this.nameStaff = "Biggerton Ouncenton III";
-    this.content = "Everything is fine! Nothing to worry about!";
-    this.period = new PeriodDTO(2025, Semester.AUG_JAN);
-    this.createdAt = LocalDateTime.now();
-  }
 
   public int getID() {
     return id;
   }
 
-  public String getContent() {
-    return content;
+  public void setID(int id) {
+    this.id = id;
+  }
+
+  public int getIDStaff() {
+    return idStaff;
+  }
+
+  public void setIDStaff(int idStaff) {
+    this.idStaff = idStaff;
+  }
+
+  public int getIDSessionPlan() {
+    return idSessionPlan;
+  }
+
+  public void setIDSessionPlan(int idSessionPlan) {
+    this.idSessionPlan = idSessionPlan;
+  }
+
+  public int getIDProgram() {
+    return idProgram;
+  }
+
+  public void setIDProgram(int idProgram) {
+    this.idProgram = idProgram;
+  }
+
+  public int getIDResponse() {
+    return idResponse;
+  }
+
+  public void setIDResponse(int idResponse) {
+    this.idResponse = idResponse;
+  }
+
+  public String getNameStaff() {
+    return nameStaff;
+  }
+
+  public void setNameStaff(String nameStaff) {
+    this.nameStaff = nameStaff;
+  }
+
+  public TutoringSessionKind getSessionKind() {
+    return sessionKind;
+  }
+
+  public void setSessionKind(TutoringSessionKind sessionKind) {
+    this.sessionKind = sessionKind;
   }
 
   public ReportType getType() {
     return type;
   }
 
+  public void setType(ReportType type) {
+    this.type = type;
+  }
+
   public ReportState getState() {
     return state;
   }
 
-  public TutoringSessionKind getTutoringSessionKind() {
-    return tutoringSessionKind;
+  public void setState(ReportState state) {
+    this.state = state;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public PeriodDTO getPeriodDTO() {
+    return periodDTO;
+  }
+
+  public void setPeriodDTO(PeriodDTO periodDTO) {
+    this.periodDTO = periodDTO;
   }
 
   @Override
@@ -55,19 +113,17 @@ public class ReportDTO implements Record {
     return createdAt;
   }
 
-  public int getIDStaff() {
-    return idStaff;
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
   }
 
-  public String getNameStaff() {
-    return nameStaff;
-  }
-
-  public PeriodDTO getPeriod() {
-    return period;
-  }
-
-  public int getIDResponse() {
-    return idResponse;
+  public String getSearchableText() {
+    return String.format("%s %s %s %s %s",
+      getState().toString(),
+      getNameStaff(),
+      getPeriodDTO().toString(),
+      getSessionKind().toString(),
+      getFormattedCreatedAt()
+    ).toLowerCase();
   }
 }
