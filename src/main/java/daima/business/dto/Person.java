218a8b41a25885012ff2b5ab3d55901d2fd2de4d
OGShawnLee <OGShawnLee@gmail.com>
2025-12-08 10:57:43 -0600

    feat: update Person

--- src/main/java/daima/business/dto/Person.java
+++ src/main/java/daima/business/dto/Person.java
@@ -1,32 +1,39 @@
 package daima.business.dto;
 
+import daima.business.validator.Validator;
+import daima.common.InvalidFieldException;
+
 import java.time.LocalDateTime;
 
-public abstract class Person implements Record {
+public abstract class Person implements Record, Searchable {
   private int id;
-  private final String name;
-  private final String lastName;
-  private final String email;
+  private String name;
+  private String lastName;
+  private String email;
   private LocalDateTime createdAt;
-
-  public Person(String name, String lastName, String email) {
-    this.id = 1;
-    this.name = name;
-    this.lastName = lastName;
-    this.email = email;
-    this.createdAt = LocalDateTime.of(2020, 6, 1, 12, 0);
-  }
 
   public int getID() {
     return id;
+  }
+
+  public void setID(int id) {
+    this.id = id;
   }
 
   public String getName() {
     return name;
   }
 
+  public void setName(String name) throws InvalidFieldException {
+    this.name = Validator.getValidFlexibleName(name, "Nombre", 2, 64);
+  }
+
   public String getLastName() {
     return lastName;
+  }
+
+  public void setLastName(String lastName) throws InvalidFieldException {
+    this.lastName = Validator.getValidFlexibleName(lastName, "Apellido", 2, 64);
   }
 
   public String getFullName() {
@@ -37,7 +44,24 @@ public abstract class Person implements Record {
     return email;
   }
 
+  public void setEmail(String email) throws InvalidFieldException {
+    this.email = Validator.getValidEmail(email);
+  }
+
   public LocalDateTime getCreatedAt() {
     return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  @Override
+  public String getSearchableText() {
+    return String.format("%s %s %s",
+      getFullName(),
+      getEmail(),
+      getFormattedCreatedAt()
+    ).toLowerCase();
   }
 }
